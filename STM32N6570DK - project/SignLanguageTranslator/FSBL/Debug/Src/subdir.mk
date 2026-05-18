672b0076ec91d9367f3eebbd04245a51a32da2bb
Oliver <grossoliver@gmx.net>
2026-05-18 18:24:32 +0200

    Added: display visual on startup with HAL

--- STM32N6570DK - project/SignLanguageTranslator/FSBL/Debug/Src/subdir.mk
+++ STM32N6570DK - project/SignLanguageTranslator/FSBL/Debug/Src/subdir.mk
@@ -37,7 +37,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m55 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32N657xx -c -I../Inc -I../../Drivers/STM32N6xx_HAL_Driver/Inc -I../../Drivers/CMSIS/Device/ST/STM32N6xx/Include -I../../Drivers/STM32N6xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -mcmse -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m55 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32N657xx -c -I../Inc -I../../Drivers/STM32N6xx_HAL_Driver/Inc -I../../Drivers/CMSIS/Device/ST/STM32N6xx/Include -I../../Drivers/STM32N6xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Include -I../../Drivers/BSP/STM32N6570-DK -I../../Drivers/BSP/Components/Common -I../../Drivers/BSP/Components/rk050hr18 -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -mcmse -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src
 
--- STM32N6570DK - project/SignLanguageTranslator/FSBL/Src/main.c
+++ STM32N6570DK - project/SignLanguageTranslator/FSBL/Src/main.c
@@ -1,127 +1,57 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include "gpio_driver.h"
-#include "lcd_driver.h"
-/* USER CODE END Includes */
+#include "smpte_163x200_YUV420_FullPlanar.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+LTDC_HandleTypeDef hltdc;
 
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
 static gpio_pin_t led1 = { .port = LED1_GPIO_Port, .pin = LED1_Pin };
-/* USER CODE END PV */
 
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
+static void MX_LTDC_Init(void);
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
+  /* Enable SRAM3 & SRAM4 clocks (framebuffer at 0x34200000 spans both) */
+  RCC->MEMENR |= RCC_MEMENR_AXISRAM3EN | RCC_MEMENR_AXISRAM4EN;
+  (void)RCC->MEMENR;
 
-  /* USER CODE BEGIN 1 */
+  /* Configure MPU: framebuffer region (0x34200000, 1 MB) as non-cacheable */
+  ARM_MPU_Disable();
+  ARM_MPU_SetMemAttr(0, 0x44U);
+  ARM_MPU_SetRegion(0,
+      ARM_MPU_RBAR(0x34200000U, ARM_MPU_SH_OUTER, 0, 0, 0),
+      ARM_MPU_RLAR(0x342FFFFFU, 0));
+  ARM_MPU_Enable(MPU_CTRL_PRIVDEFENA_Msk);
 
-  /* USER CODE END 1 */
-
-  /* Enable the CPU Cache */
-
-  /* Enable I-Cache---------------------------------------------------------*/
   SCB_EnableICache();
-
-  /* Enable D-Cache---------------------------------------------------------*/
   SCB_EnableDCache();
 
-  /* MCU Configuration--------------------------------------------------------*/
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
+  MX_GPIO_Init();
 
-  /* USER CODE BEGIN SysInit */
+  MX_LTDC_Init();
 
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  //MX_GPIO_Init();
-
-  /* USER CODE BEGIN 2 */
   gpio_init_output(&led1);
-  LCD_Init();
-  LCD_ShowTestPattern();
-  /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-	  gpio_toggle_pin(&led1);
-	  HAL_Delay(100);
-    /* USER CODE BEGIN 3 */
+    gpio_toggle_pin(&led1);
+    HAL_Delay(100);
   }
-  /* USER CODE END 3 */
 }
-/* USER CODE BEGIN CLK 1 */
-/* USER CODE END CLK 1 */
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the System Power Supply
-  */
   if (HAL_PWREx_ConfigSupply(PWR_EXTERNAL_SOURCE_SUPPLY) != HAL_OK)
   {
     Error_Handler();
   }
 
-  /** Configure the main internal regulator output voltage
-  */
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
   {
     Error_Handler();
@@ -141,9 +71,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Get current CPU/System buses clocks configuration and if necessary switch
- to intermediate HSI clock to ensure target clock can be set
-  */
   HAL_RCC_GetClockConfig(&RCC_ClkInitStruct);
   if ((RCC_ClkInitStruct.CPUCLKSource == RCC_CPUCLKSOURCE_IC1) ||
      (RCC_ClkInitStruct.SYSCLKSource == RCC_SYSCLKSOURCE_IC2_IC6_IC11))
@@ -153,14 +80,10 @@ void SystemClock_Config(void)
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
     {
-      /* Initialization Error */
       Error_Handler();
     }
   }
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
   RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL1.PLLSource = RCC_PLLSOURCE_HSI;
@@ -171,14 +94,19 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL1.PLLP2 = 1;
   RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_NONE;
   RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_NONE;
-  RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL4.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL4.PLLM = 1;
+  RCC_OscInitStruct.PLL4.PLLN = 25;
+  RCC_OscInitStruct.PLL4.PLLFractional = 0;
+  RCC_OscInitStruct.PLL4.PLLP1 = 1;
+  RCC_OscInitStruct.PLL4.PLLP2 = 1;
+
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_CPUCLK|RCC_CLOCKTYPE_HCLK
                               |RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
                               |RCC_CLOCKTYPE_PCLK2|RCC_CLOCKTYPE_PCLK5
@@ -205,53 +133,95 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
+static void MX_LTDC_Init(void)
 {
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  LTDC_LayerFlexYUVFullPlanarTypeDef pLayerFlexYUVFullPlanar = {0};
 
-  /* USER CODE END MX_GPIO_Init_1 */
+  hltdc.Instance = LTDC;
+  hltdc.Init.HSPolarity = LTDC_HSPOLARITY_AL;
+  hltdc.Init.VSPolarity = LTDC_VSPOLARITY_AL;
+  hltdc.Init.DEPolarity = LTDC_DEPOLARITY_AL;
+  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+  hltdc.Init.HorizontalSync = 4;
+  hltdc.Init.VerticalSync = 4;
+  hltdc.Init.AccumulatedHBP = 12;
+  hltdc.Init.AccumulatedVBP = 12;
+  hltdc.Init.AccumulatedActiveW = 812;
+  hltdc.Init.AccumulatedActiveH = 492;
+  hltdc.Init.TotalWidth = 820;
+  hltdc.Init.TotalHeigh = 500;
+  hltdc.Init.Backcolor.Blue = 0;
+  hltdc.Init.Backcolor.Green = 0;
+  hltdc.Init.Backcolor.Red = 0;
+  if (HAL_LTDC_Init(&hltdc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  pLayerFlexYUVFullPlanar.Layer.WindowX0 = 0;
+  pLayerFlexYUVFullPlanar.Layer.WindowX1 = 200;
+  pLayerFlexYUVFullPlanar.Layer.WindowY0 = 0;
+  pLayerFlexYUVFullPlanar.Layer.WindowY1 = 163;
+  pLayerFlexYUVFullPlanar.Layer.Alpha = 255;
+  pLayerFlexYUVFullPlanar.Layer.Alpha0 = 0;
+  pLayerFlexYUVFullPlanar.Layer.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
+  pLayerFlexYUVFullPlanar.Layer.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
+  pLayerFlexYUVFullPlanar.Layer.ImageWidth = 200;
+  pLayerFlexYUVFullPlanar.Layer.ImageHeight = 163;
+  pLayerFlexYUVFullPlanar.Layer.Backcolor.Blue = 0;
+  pLayerFlexYUVFullPlanar.Layer.Backcolor.Green = 0;
+  pLayerFlexYUVFullPlanar.Layer.Backcolor.Red = 0;
+  pLayerFlexYUVFullPlanar.FlexYUV.YUVOrder = LTDC_YUV_ORDER_CHROMINANCE_FIRST;
+  pLayerFlexYUVFullPlanar.FlexYUV.LuminanceOrder = LTDC_YUV_LUMINANCE_ORDER_EVEN_FIRST;
+  pLayerFlexYUVFullPlanar.FlexYUV.ChrominanceOrder = 0;
+  pLayerFlexYUVFullPlanar.FlexYUV.LuminanceRescale = LTDC_YUV_LUMINANCE_RESCALE_DISABLE;
+  pLayerFlexYUVFullPlanar.YUVFullPlanarAddress.YAddress = (uint32_t) smpte_163x200_YUV420_FullPlanar;
+  pLayerFlexYUVFullPlanar.YUVFullPlanarAddress.UAddress = (uint32_t)smpte_163x200_YUV420_FullPlanar + (width*height);
+  pLayerFlexYUVFullPlanar.YUVFullPlanarAddress.VAddress = (uint32_t)smpte_163x200_YUV420_FullPlanar + (width*height) + (width*height >> 2);
+  pLayerFlexYUVFullPlanar.ColorConverter = LTDC_YUV2RGBCONVERTOR_BT709_FULL_RANGE;
+  if (HAL_LTDC_ConfigLayerFlexYUVFullPlanar(&hltdc, &pLayerFlexYUVFullPlanar, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  RIMC_MasterConfig_t RIMC_master = {0};
+  RIMC_master.MasterCID = RIF_CID_1;
+  RIMC_master.SecPriv = RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV;
+  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_LTDC1 , &RIMC_master);
 
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_LTDCL1 , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
 }
 
-/* USER CODE BEGIN 4 */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-/* USER CODE END 4 */
+  __HAL_RCC_GPIOQ_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+  HAL_GPIO_WritePin(GPIOQ, LCD_BL_CTRL_Pin|LCD_ONOFF_Pin, GPIO_PIN_SET);
+
+  GPIO_InitStruct.Pin = LCD_BL_CTRL_Pin|LCD_ONOFF_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOQ, &GPIO_InitStruct);
+}
+
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif
