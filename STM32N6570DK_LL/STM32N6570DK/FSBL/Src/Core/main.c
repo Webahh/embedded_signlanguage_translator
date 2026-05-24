b3a0242a2d678bb56b4d4877bc3b8846f206413a
Oliver <grossoliver@gmx.net>
2026-05-24 13:16:18 +0200

    feat: Scheduler, GPIO, LCD background working

--- STM32N6570DK_LL/STM32N6570DK/FSBL/Src/Core/main.c
+++ STM32N6570DK_LL/STM32N6570DK/FSBL/Src/Core/main.c
@@ -7,19 +7,39 @@
 
 #define LED2_PIN 10
 
-static uint8_t bg_color_state = 0;
+#define BG_NUM_COLORS 3
+
+static const uint8_t bg_colors[BG_NUM_COLORS][3] = {
+    {255, 0, 0},    /* Red   */
+    {0, 255, 0},    /* Green */
+    {0, 0, 255}     /* Blue  */
+};
+
+static uint8_t  bg_seg_idx    = 0;
+static uint32_t bg_blend_start = 0;
 
 static void vLEDTask(void) {
 	GPIO_BSRR_Toggle(GPIOG, LED2_PIN);
 }
 
 static void vBackgroundTask(void) {
-	switch (bg_color_state) {
-		case 0: LCD_SetBackgroundColor(255, 0, 0); break;
-		case 1: LCD_SetBackgroundColor(0, 255, 0); break;
-		case 2: LCD_SetBackgroundColor(0, 0, 255); break;
-	}
-	bg_color_state = (bg_color_state + 1) % 3;
+    uint32_t now     = SCHEDULER_GetTick();
+    uint32_t elapsed = now - bg_blend_start;
+
+    if (elapsed >= 1000) {
+        bg_seg_idx = (bg_seg_idx + 1) % BG_NUM_COLORS;
+        bg_blend_start = now;
+        elapsed = 0;
+    }
+
+    uint8_t next_idx = (bg_seg_idx + 1) % BG_NUM_COLORS;
+    uint32_t t       = elapsed;
+
+    uint8_t r = (uint8_t)(((uint32_t)bg_colors[bg_seg_idx][0] * (1000 - t) + (uint32_t)bg_colors[next_idx][0] * t) / 1000);
+    uint8_t g = (uint8_t)(((uint32_t)bg_colors[bg_seg_idx][1] * (1000 - t) + (uint32_t)bg_colors[next_idx][1] * t) / 1000);
+    uint8_t b = (uint8_t)(((uint32_t)bg_colors[bg_seg_idx][2] * (1000 - t) + (uint32_t)bg_colors[next_idx][2] * t) / 1000);
+
+    LCD_SetBackgroundColor(r, g, b);
 }
 
 int main(void){
@@ -31,7 +51,7 @@ int main(void){
 	SCHEDULER_Init();
 
 	SCHEDULER_AddTask(vLEDTask, "LED", 500);
-	SCHEDULER_AddTask(vBackgroundTask, "BgColor", 500);
+	SCHEDULER_AddTask(vBackgroundTask, "BgColor", 20);
 
 	while (1) {
 		SCHEDULER_Run();
