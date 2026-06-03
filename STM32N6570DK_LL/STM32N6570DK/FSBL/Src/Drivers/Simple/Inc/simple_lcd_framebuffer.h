b4959e134b95a01215ea64ca77c42adc4e62aab4
OliverCro <grossoliver@gmx.net>
2026-06-03 12:26:30 +0200

    bug: blending between LTDC layers causes this error

--- STM32N6570DK_LL/STM32N6570DK/FSBL/Src/Drivers/Simple/Inc/simple_lcd_framebuffer.h
+++ STM32N6570DK_LL/STM32N6570DK/FSBL/Src/Drivers/Simple/Inc/simple_lcd_framebuffer.h
@@ -13,8 +13,8 @@
 #define LCD_BG_WIDTH  600
 #define LCD_BG_HEIGHT 480
 
-#define LCD_FG_WIDTH  20
-#define LCD_FG_HEIGHT 20
+#define LCD_FG_WIDTH  40
+#define LCD_FG_HEIGHT 40
 
 #define LCD_BYTES_PER_PIXEL 4U
 
