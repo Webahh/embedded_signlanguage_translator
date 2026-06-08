505150f259e53e3166c925fa088a6aacf0458826
OliverCro <grossoliver@gmx.net>
2026-06-08 18:52:50 +0200

    fix: dcmipp downscale fixed

--- embedded/STM32N6570DK/FSBL/Src/Drivers/Simple/Src/simple_camera.c
+++ embedded/STM32N6570DK/FSBL/Src/Drivers/Simple/Src/simple_camera.c
@@ -85,7 +85,7 @@ CAM_Status CAM_Init(CAM_Handle *h, uint32_t nn_buf)
     csi_conf.data_lane_mapping = CSI_DATA_LANES_PHYSICAL;
     csi_conf.phy_bitrate       = CSI_PHY_BT_1600;
     csi_conf.vc                = CSI_VIRTUAL_CHANNEL0;
-    csi_conf.dt_format         = CSI_DT_BPP8;
+    csi_conf.dt_format         = CSI_DT_BPP10;
     csi_conf.data_type         = 0x2B;
     CSI_Config(&csi_conf);
     CSI_SetVCConfig(csi_conf.vc, csi_conf.dt_format);
@@ -163,7 +163,7 @@ CAM_Status CAM_Init(CAM_Handle *h, uint32_t nn_buf)
     /* ------ ISP ------ */
 
     DCMIPP_Pipe_EnableISP(CAM_PIPE_DISPLAY, DCMIPP_RAWBAYER_RGGB);
-    DCMIPP_Pipe_SetBlackLevel(CAM_PIPE_DISPLAY, 64, 64, 64);
+    DCMIPP_Pipe_SetBlackLevel(CAM_PIPE_DISPLAY, 0x0, 0x0, 0x0);
     DCMIPP_Pipe_EnableBlackLevel(CAM_PIPE_DISPLAY);
 
     /* ------ Sensor init ------ */
